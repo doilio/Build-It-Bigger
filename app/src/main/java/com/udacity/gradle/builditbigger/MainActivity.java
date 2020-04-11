fc11eb459d0a51fb4f758c6d377b527fb9f0cd42
doilio <doiliomatsinhe@hotmail.com>
2020-04-11 08:07:11 +0200

    Create a Java Library
    Signed-off-by: doilio <doiliomatsinhe@hotmail.com>

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.doiliomatsinhe.lib.Joker;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Joker joker = new Joker();
+        String joke = joker.getJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_LONG).show();
     }
 
 
---
+++ lib/src/main/java/com/doiliomatsinhe/lib/Joker.java
@@ -0,0 +1,7 @@
+package com.doiliomatsinhe.lib;
+
+public class Joker {
+    public String getJoke() {
+        return "Why can't bicycles stand up on their own?\nAns: They are two tired.";
+    }
+}
