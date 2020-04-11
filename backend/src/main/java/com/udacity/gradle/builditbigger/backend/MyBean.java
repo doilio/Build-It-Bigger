804e992389d5fdfb6e48d8b53823a8e5a8898353
doilio <doiliomatsinhe@hotmail.com>
2020-04-11 11:22:07 +0200

    Connect with GCE
    Signed-off-by: doilio <doiliomatsinhe@hotmail.com>

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyBean.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyBean.java
@@ -1,14 +1,23 @@
 package com.udacity.gradle.builditbigger.backend;
 
-/** The object model for the data we are sending through endpoints */
+import com.doiliomatsinhe.lib.Joker;
+
+/**
+ * The object model for the data we are sending through endpoints
+ */
 public class MyBean {
 
     private String myData;
 
     public String getData() {
+        myData = new Joker().getJoke();
         return myData;
     }
 
+    /**
+     * Remove this later as it will not be needed
+     * @param data
+     */
     public void setData(String data) {
         myData = data;
     }
